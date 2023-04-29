d9bf9941a9229c64d5251478326748aebdd9d8d7
janaka <janakaud@gmail.com>
2023-04-29 16:43:40 +0530

    Support pom-level repositories

--- src/main/java/com/tuna/MirrorFallbackMavenRepositorySystem.java
+++ src/main/java/com/tuna/MirrorFallbackMavenRepositorySystem.java
@@ -4,6 +4,7 @@ import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.bridge.MavenRepositorySystem;
 import org.apache.maven.settings.Mirror;
 import org.codehaus.plexus.component.annotations.Component;
+import org.eclipse.aether.RepositorySystemSession;
 
 import java.util.List;
 
@@ -12,6 +13,16 @@ public class MirrorFallbackMavenRepositorySystem extends MavenRepositorySystem {
     @Override
     public void injectMirror(List<ArtifactRepository> repos, List<Mirror> mirrors) {
         super.injectMirror(repos, mirrors);
+        restoreRepos(repos);
+    }
+
+    @Override
+    public void injectMirror(RepositorySystemSession session, List<ArtifactRepository> repos) {
+        super.injectMirror(session, repos);
+        restoreRepos(repos);
+    }
+
+    private void restoreRepos(List<ArtifactRepository> repos) {
         for (int i = 0; i < repos.size(); i++) {
             ArtifactRepository mirror = repos.get(i);
             for (ArtifactRepository orig : mirror.getMirroredRepositories()) {
